3e86bff455db6b0c7d9353eb96f0d91b30854d74
Adrià Prat <adria@adriaprat.com>
2017-09-13 15:02:06 +0200

    Add header comments

--- validate/validate.py
+++ validate/validate.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""Validates Apple's in-app purchase receipt.
+
+Uses itunesiap package to validate receipts.
+Said receipts must be in a file
+passed as an argument.
+"""
 
 import itunesiap
 import optparse
 import os.path
 
+__author__ = "Adrià Prat"
+__copyright__ = "Copyright 2017, Adrià Prat"
+__credits__ = "Adrià Prat"
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Adrià Prat"
+__email__ = "adria@adriaprat.com"
+__status__ = "Development"
+
 def validate():
+    # Setup parser
     p = optparse.OptionParser(
         description=' Validate iTunes in-app purchase receipt',
         prog='validate.py',
@@ -15,14 +32,13 @@ def validate():
     if len(arguments) == 1 and os.path.exists(arguments[0]):
         f = open(arguments[0])
         try:
-            """ base64-encoded data """
+            # base64-encoded data from file argument
             response = itunesiap.verify(f.read())
-            print response.receipt.last_in_app.product_id 
+            print response.receipt.last_in_app.product_id
         except itunesiap.exc.InvalidReceipt as e:
             print 'invalid receipt: ' + e.description
     else:
         p.print_help()
 
-if __name__=="__main__":
+if __name__=="__main__":    #code to execute if called from command-line
 	validate()
-
